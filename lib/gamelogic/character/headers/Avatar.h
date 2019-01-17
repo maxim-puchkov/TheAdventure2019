31da4cf89a1bdaedc2ccf9c73a7414606be0f56b
mpuchkov <mpuchkov@sfu.ca>
2019-01-17 14:55:15 -0800

    header files for character

---
+++ lib/gamelogic/character/headers/Avatar.h
@@ -0,0 +1,21 @@
+/////////////////////////////////////////////////////////////////////////////
+//                                  Avatar
+//
+// This file is distributed under the MIT License. See the LICENSE file
+// for details.
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef Avatar_h
+#define Avatar_h
+
+#include <string>
+#include "Character.h"
+
+class Avatar : public PlayerControllerCharacter {
+public:
+    
+private:
+    
+};
+
+#endif /* Avatar_h */
---
+++ lib/gamelogic/character/headers/PlayerControlledCharacter.h
@@ -0,0 +1,21 @@
+//
+//  PlayerControlledCharacter.h
+//  test
+//
+//  Created by admin on 2019-01-17.
+//  Copyright © 2019 maximpuchkov. All rights reserved.
+//
+
+#ifndef PlayerControlledCharacter_h
+#define PlayerControlledCharacter_h
+
+class PlayerControlledCharacter : public Character {
+public:
+
+	virtual ~PlayerControlledCharacter() { }
+
+protected:
+
+};
+
+#endif /* PlayerControlledCharacter_h */
