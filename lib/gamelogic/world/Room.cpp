eb75da28b8b26515b6ef1cf04a461d7f3e172e2e
jjankows <jjankows@sfu.ca>
2019-01-17 21:55:12 -0800

    Added body of WorldManager, Room, and Exit classes

--- lib/gamelogic/world/Room.cpp
+++ lib/gamelogic/world/Room.cpp
@@ -1,39 +1,91 @@
-#include <string> //maybe
+#include <string>
 #include <vector>
+//#include <algorithm> //std::any_of
+#include <utility> //std::move (objects)
 #include "Exit.h"
 #include "Character.h"
+#include "Item.h"
 
 class Room {
 	
 	private:
-		Exit[4] roomExits;
 		std::string roomName;
 		std::string roomDescription;
 		std::vector<Character*> charactersInRoom;
-		std::vector<Item*> itemsInRoom;
+		//std::vector<Item> itemsInRoom;
+		std::vector<Exit> roomExits;
 
 	public:
-		unsigned int move(Character* character, short direction) const;
-		std::string look() const;
+		Room()
+			: roomName{"NO_ROOM_NAME"},
+			roomDescription{"NO_ROOM_DESCRIPTION"}
+		{}
+		Room(std::string rName, std::string rDescription)
+			: roomName{rName},
+			roomDescription{rDescription}
+		{}
+		int move(Character* character, short direction);
+		bool createExit();
 		bool addCharacter(Character* character);
 		bool removeCharacter(Character* character);
-
+		//getters
+		std:string getName() const { return roomName; }
+		std:string getDescription() const { return roomDescription; }
+		//setters
+		void setName(std::string newName) { roomName = newName; }
+		void setDescription(std::string newDescription) { roomDescription = newDescription; }
 };
 
-int Room::move(Character* character, short direction){
+// Returns -1 if move is not valid.
+// Otherwise, removes character from charactersInRoom and returns ID of new room.
+int Room::move(*Character character, short direction){
+	if(roomExits.size() <= 0) return -1;
+	for (auto exit : roomExits){
+		if(exit.getDirection == direction){
+			removeCharacter(character);
+			return exit.getTargetID();
+		}
+	}
+	/* Fancier implementation
+	if( std::any_of( roomExits.begin(), roomExits.end(),
+	 	[](auto exit) { return exit.getDirection == direction } ) ){
+
+	 	removeCharacter(character);
+	 	return exit.getTargetID();
+	}
+	 */
 	return -1;
 }
 
-std::string Room::look(){
-	return roomDescription;
-}
+bool Room::createExit(std::string exitName, std::string exitDescription,
+						short cardinalDirection, int targetRoomID) {
 
-bool Room::addCharacter(Character* character){
-	charactersInRoom.push_back(character);
-	//implement better return or exception
+	Exit newExit(exitName, exitDescription, cardinalDirection, targetRoomID);
+	try{
+		roomExits.push_back( std::move(newExit) );
+	} catch(const std::bad_alloc& e){
+		return false;
+	}
 	return true;
 }
 
-bool Room::removeCharacter(Character* character){
+bool Room::addCharacter(*Character character){
+	try{
+		charactersInRoom.push_back(character);
+	} catch(const std::bad_alloc& e){
+		return false;
+	}
+	return true;
+}
+
+bool Room::removeCharacter(*Character character){
+	int i = 0;
+	for (auto *listCharacter : charactersInRoom){
+		if(character == listCharacter){
+			charactersInRoom.erase(charactersInRoom.begin() + i);
+			return true;
+		}
+		i++;
+	}
 	return false;
 }
--- lib/gamelogic/world/WorldManager.cpp
+++ lib/gamelogic/world/WorldManager.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <cstdlib> //std::rand
 #include "Room.h"
 #include "Character.h"
 
@@ -15,17 +16,29 @@ class WorldManager {
 };
 
 worldManager(){
+	//temporary hard coded rooms
 	for(int i = 0; i < 10; i++){
-		Room r;
+		std::string roomName = "Room number " + std::to_string(i+1);
+		std::string roomDescription = "This dark room contains only the number " + std::to_string(i+1);
+		int exit1TargetID = std::rand() % (i+1) //rand ID from 0 to i
+		int exit2TargetID = std::rand() % (i+1) //rand ID from 0 to i
+
+		Room r(roomName, roomDescription);
+		r.createExit("Exit 1", "The first exit. Looks shady.", 0, exitTargetID);
+		r.createExit("Exit 2", "The second exit. Looks way better than exit 1.", 0, exitTargetID);
 		worldRooms[i] = r;
 	}
-	//generate rooms
 }
 
 int WorldManager::move(Character* character, short direction){
-		return 0;
+	int currentRoomID = character->roomID;
+	int newRoomID = worldRooms[currentRoomID].move(character, direction);
+	if( newRoomID < 0) return -1; //if move failed, return
+
+	character->roomID = newRoomID; //update character's stored position
+	worldRooms[newRoomID].addCharacter(character); //add character to new room in world
 }
 
 std::string WorldManager::look(unsigned int roomID){
-		return "The room is dark, cold and empty.";
+		return worldRooms[roomID].getDescription();
 }
