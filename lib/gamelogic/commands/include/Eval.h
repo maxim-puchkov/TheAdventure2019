192948083565cb8bafeb3ea06431fa6dcd8fc939
Maxim Puchkov <mpuchkov@sfu.ca>
2019-01-29 10:23:33 -0800

    Implementing function that can invoke any other function

---
+++ lib/gamelogic/commands/include/Eval.h
@@ -0,0 +1,36 @@
+//
+//  Eval.h
+//  tests
+//
+//  Created by admin on 2019-01-29.
+//  Copyright © 2019 maximpuchkov. All rights reserved.
+//
+
+#ifndef Eval_h
+#define Eval_h
+
+//template<template<class A, typename B> class E, typename  S>
+//class Eval { };
+//
+//
+//template<class A, typename ...B>
+//template<class std::function<A(B...)>>
+//class Eval<A(B &&...)> {
+//public:
+//    
+//    template<template<typename, typename> class F, typename ...B>
+//    void fold(F &&f, B &&...bs) {
+//       auto = f(std::forward<decltype(bs)>...);
+//        std::cout << std::endl;
+//    }
+//    
+//    Eval() {};
+//    
+//    
+//
+//    
+//    
+//};
+
+
+#endif /* Eval_h */
