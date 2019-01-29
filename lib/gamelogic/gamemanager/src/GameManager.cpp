6381d1a1acceb1ed67166d577ed1846f9ff209ae
tnpham@sfu.ca <tnpham@sfu.ca>
2019-01-29 07:19:18 -0800

    Refactored, finalized Help command, added support for upper and lower case commands

--- lib/gamelogic/gamemanager/src/GameManager.cpp
+++ lib/gamelogic/gamemanager/src/GameManager.cpp
@@ -11,15 +11,15 @@ GameManager::GameManager() {
 }
 
 void GameManager::createTableOfCommands() {
-    commandGuideline login = {&GameManager::commandLogin, &GameManager::commandError, 2, 0, " username password"};
+    commandGuideline login = {&GameManager::commandLogin, &GameManager::commandError, 2, 0, " [username] [password]"};
     commandGuideline logout = {&GameManager::commandLogout, &GameManager::commandError, 0, 0, ""};
-    commandGuideline create = {&GameManager::commandCreate, &GameManager::commandError, 2, 0, " username password"};
-    commandGuideline say = {&GameManager::commandAddToActionList, &GameManager::commandSay, 0, 1, ": message"};
-    commandGuideline yell = {&GameManager::commandAddToActionList, &GameManager::commandYell, 0, 1, ": message"};
-    commandGuideline tell = {&GameManager::commandAddToActionList, &GameManager::commandTell, 1, 1, " other-username: message"};
-    commandGuideline move = {&GameManager::commandAddToActionList, &GameManager::commandMove, 1, 0, " direction"};
+    commandGuideline create = {&GameManager::commandCreate, &GameManager::commandError, 2, 0, " [username] [password]"};
+    commandGuideline say = {&GameManager::commandAddToActionList, &GameManager::commandSay, 0, 1, ": [message]"};
+    commandGuideline yell = {&GameManager::commandAddToActionList, &GameManager::commandYell, 0, 1, ": [message]"};
+    commandGuideline tell = {&GameManager::commandAddToActionList, &GameManager::commandTell, 1, 1, " [other-username]: [message]"};
+    commandGuideline move = {&GameManager::commandAddToActionList, &GameManager::commandMove, 1, 0, " [direction]"};
     commandGuideline look = {&GameManager::commandAddToActionList, &GameManager::commandLook, 0, 0, ""};
-    commandGuideline examine = {&GameManager::commandAddToActionList, &GameManager::commandExamine, 1, 0, " object/username"};
+    commandGuideline examine = {&GameManager::commandAddToActionList, &GameManager::commandExamine, 1, 0, " [object/username]"};
     commandGuideline help = {&GameManager::commandHelp, &GameManager::commandError, 0, 0, ""};
     
     tableOfCommands.insert({"log-in", login});
@@ -34,15 +34,17 @@ void GameManager::createTableOfCommands() {
     tableOfCommands.insert({"help", help});
 }
 
+//tell a : a;
+
 std::string GameManager::extractCommands(const std::string& connectionID, std::string fullCommand) {
     std::vector<std::string> commandParts, splitByColon;
-    breakdownCommand(fullCommand, commandParts, splitByColon);
+    reassembleCommand(fullCommand, commandParts, splitByColon);
 
     auto found = tableOfCommands.find(commandParts[0]);
     if(found != tableOfCommands.end()) {
         commandGuideline guideline = found->second;
-        if (commandIsValid(commandParts.size() - 1, splitByColon.size() - 1, guideline))
-            return (this->*guideline.promptReply)(connectionID, fullCommand);
+        if (commandIsValid(commandParts.size(), splitByColon.size(), guideline))
+            return (this->*guideline.promptReply)(connectionID, commandParts);
         else {
             std::ostringstream answer;
             answer << "Invalid command. Command syntax: " << commandParts[0] << guideline.helpText << "\n";
@@ -52,7 +54,7 @@ std::string GameManager::extractCommands(const std::string& connectionID, std::s
     return "Command not found";
 }
 
-void GameManager::breakdownCommand(std::string fullCommand, std::vector<std::string>& commandParts, std::vector<std::string>& splitByColon) {
+void GameManager::reassembleCommand(std::string fullCommand, std::vector<std::string>& commandParts, std::vector<std::string>& splitByColon) {
     boost::trim_if(fullCommand, boost::is_any_of(" \t"));
     boost::split(splitByColon, fullCommand, boost::is_any_of(":"), boost::token_compress_on);
    
@@ -60,74 +62,87 @@ void GameManager::breakdownCommand(std::string fullCommand, std::vector<std::str
         boost::trim_if(text, boost::is_any_of(" \t"));
     }
     boost::split(commandParts, splitByColon[0], boost::is_any_of(" \t"), boost::token_compress_on);
+
+    for(auto& text : splitByColon) {
+        commandParts.push_back(text);
+    }
 }
 
-bool GameManager::commandIsValid(size_t commandPartsSize, size_t messagePartsSize, commandGuideline guideline) {
-    if(guideline.commandPartArgCount != commandPartsSize)
+bool GameManager::commandIsValid(size_t commandPartsSize, size_t splitByColon, commandGuideline guideline) {
+    if(guideline.commandPartArgCount != commandPartsSize - 1 - splitByColon)
         return false;
-    if(guideline.messagePartArgCount != messagePartsSize)
+    if(guideline.messagePartArgCount != splitByColon - 1)
         return false;
     return true;
 }
 
-std::string GameManager::commandLogin(std::string connectionID, std::string fullCommand) {
-    return "test";
+std::string GameManager::commandLogin(std::string connectionID, std::vector<std::string> fullCommand) {
+    return "log-in test";
 }
 
-std::string GameManager::commandLogout(std::string connectionID, std::string fullCommand) {
-    return "test";
+std::string GameManager::commandLogout(std::string connectionID, std::vector<std::string> fullCommand) {
+    return "log-out test";
 }
 
-std::string GameManager::commandCreate(std::string connectionID, std::string fullCommand) {
-    return "test";
+std::string GameManager::commandCreate(std::string connectionID, std::vector<std::string> fullCommand) {
+    return "create-acc test";
 }
 
-std::string GameManager::commandAddToActionList(std::string connectionID, std::string fullCommand) {
-    return "test";
+std::string GameManager::commandAddToActionList(std::string connectionID, std::vector<std::string> fullCommand) {
+    return "add-to-action-queue test";
 }
 
-std::string GameManager::commandHelp(std::string connectionID, std::string fullCommand) {
-    return "test";
+std::string GameManager::commandHelp(std::string connectionID, std::vector<std::string> fullCommand) {
+    std::ostringstream answer;
+    answer << "--------------------------------------------------\n";
+    answer << "Supported commands: \n";
+    for (const auto& [command, guideline] : tableOfCommands) {
+        answer << command << guideline.helpText << std::endl;
+}   
+    return answer.str();
 }
 
-void GameManager::commandSay(User* user, std::string fullCommand) {
-
-}
-
-void GameManager::commandYell(User* user, std::string fullCommand) {
-
-}
-
-void GameManager::commandTell(User* user, std::string fullCommand){
-
-}
-
-void GameManager::commandMove(User* user, std::string fullCommand) {
+void GameManager::commandSay(User* user, std::vector<std::string> fullCommand) {
 
 }
 
-void GameManager::commandLook(User* user, std::string fullCommand) {
+void GameManager::commandYell(User* user, std::vector<std::string> fullCommand) {
 
 }
 
-void GameManager::commandExamine(User* user, std::string fullCommand) {
+void GameManager::commandTell(User* user, std::vector<std::string> fullCommand){
 
 }
 
-void GameManager::commandError(User* user, std::string fullCommand){
+void GameManager::commandMove(User* user, std::vector<std::string> fullCommand) {
 
+}
+
+void GameManager::commandLook(User* user, std::vector<std::string> fullCommand) {
+
+}
+
+void GameManager::commandExamine(User* user, std::vector<std::string> fullCommand) {
+
+}
+
+void GameManager::commandError(User* user, std::vector<std::string> fullCommand){
+    //Intended to be a null-function. Normally it should never reach this.
 }
 
 
 
 
-void GameManager::heartbeat() const {
+std::unordered_map<std::string, std::string> GameManager::heartbeat() {
 	//fill this
+    std::unordered_map<std::string, std::string> heartbeatMessagesToUsers;
+    return heartbeatMessagesToUsers;
 }
 
 User* GameManager::getUser(const std::string userName) const {
+    //User* user = AccountManager::getOnlineUser(userName);
+    //return user; //Note: nullptr = not online, processed in the upper level
 	return nullptr;
-	//need function User* getUserByUsername(string username) in UserManager API
 }
 /*
 void testOnlineUser(){
