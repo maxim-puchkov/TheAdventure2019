7e9706cfe6830e1eb56ba76f4f724b7b70a50ed8
Maxim Puchkov <mpuchkov@sfu.ca>
2019-02-04 12:21:47 -0800

    Moved to be used in character's scripts

---
+++ lib/scripts/include/CommandParser.h
@@ -0,0 +1,48 @@
+/////////////////////////////////////////////////////////////////////////////
+//                                  CommandParser
+//
+// This file is distributed under the MIT License. See the LICENSE file
+// for details.
+/////////////////////////////////////////////////////////////////////////////
+
+#include <queue>
+#include <vector>
+#include <string>
+#include <regex>
+#include <iostream>
+#include <sstream>
+
+#include "Environment.h"
+
+
+
+using std::vector;
+using std::string;
+
+
+using arguments = const vector<string>&;
+using command = std::function<void(arguments)>;
+
+
+const string CMD_HELP = "help";
+const string CMD_NOT_FOUND = "Invalid command. See available: " + CMD_HELP;
+
+
+class CommandParser {
+public:
+    
+    CommandParser();
+    
+    ~CommandParser();
+    
+    void parse(const string &input) const;
+    
+    void createCommand(const string &name, command function);
+    
+private:
+    
+    static void init(CommandParser *p);
+    
+    Environment<string, command> env;
+    
+};
---
+++ lib/scripts/src/CommandParser.cpp
@@ -0,0 +1,71 @@
+/////////////////////////////////////////////////////////////////////////////
+//                                  CommandParser
+//
+// This file is distributed under the MIT License. See the LICENSE file
+// for details.
+/////////////////////////////////////////////////////////////////////////////
+
+#include "CommandParser.h"
+#include "CommandDefinitions.h"
+
+
+CommandParser::CommandParser() { CommandParser::init(this); }
+
+
+CommandParser::~CommandParser() { }
+
+
+void CommandParser::parse(const string &input) const {
+    try {
+        size_t position = input.find(" ");
+        string name = input.substr(0, position);
+        
+        
+        std::istringstream text(input.substr(position + 1));
+        vector<string> arguments{std::istream_iterator<string>{text},
+                                 std::istream_iterator<string>{}};
+
+        /* Display arguments to check
+        for (auto& arg : arguments) {
+            std::cout << arg << std::endl;
+        }
+        */
+        
+        command fn = this->env.lookup(name);
+        fn(arguments);
+    } catch (std::invalid_argument &e) {
+        // return CMD_NOT_FOUND;
+    }
+}
+
+void CommandParser::createCommand(const string &name, command function) {
+    this->env.bind(name, function);
+}
+
+
+
+
+
+// Private
+
+
+
+void CommandParser::init(CommandParser *p) {
+    p->createCommand("login",    &accounts::command_login);
+    p->createCommand("logout",   &accounts::command_logout);
+    
+    /*
+    p->createCommand("login", &test::exampleLogin, 2);          // login <bob> <123>
+    p->createCommand("help", &test::exampleShowHelp, 0);        // help
+    p->createCommand("error", &test::exampleThrowCustomErr, 1); // error <bob>
+    p->createCommand("tell", &test::exampleTell, 3);            // tell <bob> <msg>
+    p->createCommand("say", &test::exampleShowWorldSay, 0);     // say
+    p->createCommand("move", &test::exampleWorldMove, 1);       // move <dir>
+    p->createCommand("look", &test::exampleShowWorldLook, 1);   // look <obj>
+     */
+    
+}
+
+
+
+
