6b46ea712cc55cc77c485e236d1d6f2c64b4fcae
IanChang <ica29@sfu.ca>
2019-03-08 19:13:09 -0800

    attribute tests for Character

--- test/CharacterTest.cpp
+++ test/CharacterTest.cpp
@@ -3,7 +3,31 @@
 #include "Character.h"
 
 TEST(CharacterTests, CreatingCharacter) {
-    Character *test;
-    //test->createCharacter("tester");
+    std::string username = "tester";
+    Character testCharacter;
+    testCharacter.createCharacter(username);
+    
+    EXPECT_EQ(username, testCharacter.getName());
+}
 
+TEST(CharacterTests, GetBaseAttributes) {
+    std::string username = "tester";
+    Character testCharacter;
+    testCharacter.createCharacter(username);
+
+    Attributes testAttributes;
+
+    EXPECT_EQ(testAttributes.getHealth(), (testCharacter.getBaseAttributes()).getHealth());
+}
+
+TEST(CharacterTests, CharacterAliveOrDeadDependingOnHealth) {
+    std::string username = "tester";
+    Character testCharacter;
+    testCharacter.createCharacter(username);
+
+    // Check if created character is created alive
+    EXPECT_GT(0, (testCharacter.getAttributes()).getHealth());
+    EXPECT_TRUE(testCharacter.isAlive());
+
+    // WIP: add part that deals damage and kills character then checks if isAlive = false
 }
