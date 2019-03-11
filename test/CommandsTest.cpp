5fc9c07fef7e0ff0d1c680d9fb9eaef2837a9172
IanChang <ica29@sfu.ca>
2019-03-10 22:05:40 -0700

    added test for commands, currently don't pass

--- test/CommandsTest.cpp
+++ test/CommandsTest.cpp
@@ -2,14 +2,59 @@
 #include "gmock/gmock.h"
 #include "GameManager.h"
 
-TEST(CommandsTest, SayCommandFound) {
-    std::string fakeUser = "fakeuser";
-    std::string commandNotFoundString = "Command not found\n";
+GameManager testGameManager{};
+std::string fakeUser = "fakeUser";
+std::string commandNotCorrectSyntaxString = "Wrong command syntax. Please enter \"help\" to see the syntax.\n";
+std::string commandNotFoundString = "Command not found. Please enter \"help\" to see the syntax.\n";
+std::string commandUserNotOnlineString = "User is not online.\n";
+
+std::vector<std::string> listOfCommands {
+                                        "login",
+                                        "logout",
+                                        "create-account",
+                                        "help",
+                                        "say",
+                                        "tell",
+                                        "yell",
+                                        "look",
+                                        "examine",
+                                        "move",
+                                        "create-avatar",
+                                        "edit-avatar",
+                                        "create-room",
+                                        "edit-room",
+                                        "use",
+                                        "equip",
+                                        "pickup",
+                                        "drop",
+                                        "put",
+                                        "minigame",
+                                        "combat",
+                                        "attack",
+                                        "flee",
+                                        "cast"
+                                        };
+
+TEST(CommandsTest, CommandNotCorrectSyntax) {
+    std::string testCommand1 = "say yes :";
+    EXPECT_EQ(commandNotCorrectSyntaxString, testGameManager.extractCommands(fakeUser, testCommand1));
+}
+
+TEST(CommandsTest, CommandsSuccessfullyRecognized) {
+    for (int i = 0; i < listOfCommands.size(); i++) {
+        EXPECT_NE(commandNotFoundString, testGameManager.extractCommands(fakeUser, listOfCommands[i]));
+    }
+}
+
+TEST(CommandsTest, SayCommand) {
     std::string testCommand1 = "say: test";
     std::string testCommand2 = "      sAy     :    ssdw     a  test ";
-    std::string testCommand3 = "fail test";
-    GameManager testObject{};
-    EXPECT_NE(commandNotFoundString, testObject.extractCommands(fakeUser, testCommand1));
-    EXPECT_NE(commandNotFoundString, testObject.extractCommands(fakeUser, testCommand2));
-    EXPECT_EQ(commandNotFoundString, testObject.extractCommands(fakeUser, testCommand3));
+    std::string testCommand3 = "SAY: testSAY:";
+    std::string testCommand4 = "say: say:";
+    std::string testCommand5 = "say: tell:";
+    EXPECT_EQ(commandUserNotOnlineString, testGameManager.extractCommands(fakeUser, testCommand1));
+    EXPECT_EQ(commandUserNotOnlineString, testGameManager.extractCommands(fakeUser, testCommand2));
+    EXPECT_EQ(commandUserNotOnlineString, testGameManager.extractCommands(fakeUser, testCommand3));
+    EXPECT_EQ(commandUserNotOnlineString, testGameManager.extractCommands(fakeUser, testCommand4));
+    EXPECT_EQ(commandUserNotOnlineString, testGameManager.extractCommands(fakeUser, testCommand5));
 }
