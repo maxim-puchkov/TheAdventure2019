fb2f04ad3c5ef0bdbbf050b78e2382e3ffc5417d
IanChang <ica29@sfu.ca>
2019-03-15 17:58:26 -0700

    deleted tests

--- test/CommandsTest.cpp
+++ test/CommandsTest.cpp
@@ -1,17 +1,15 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 #include "GameManager.h"
+#include "Command.h"
+
+using usermanager::OnlineUserManager;
+using charactermanager::CharacterManager;
 
 struct CommandsTest : testing::Test {
     GameManager testGameManager{};
     std::string testUserName = "testUserName19238021";
     std::string testUserPassword = "testUserPassword";
-    std::string commandNotCorrectSyntaxString =
-        "Wrong command syntax. Please enter \"help\" to see the syntax.\n";
-    std::string commandNotFoundString =
-        "Command not found. Please enter \"help\" to see the syntax.\n";
-    std::string commandUserNotOnlineString =
-        "User is not online.\n";
     std::vector<std::string> listOfCommands {
                                         "login",
                                         "logout",
@@ -40,13 +38,20 @@ struct CommandsTest : testing::Test {
                                         };
 };
 
-class MockGameManager : public GameManager {
+class MockCommandSay : public CommandSay {
 public:
-    MOCK_METHOD2(extractCommands, std::string(const std::string& connectionID, std::string fullCommand));
+    MockCommandSay(CharacterManager& c, OnlineUserManager& u, WorldManager& w) : CommandSay(c, u, w) {}
+    MOCK_METHOD2(reassembleCommand, std::vector<std::string>(std::string& fullCommand, bool& commandIsValid));
 };
 
+// Test does not work correctly (EXPECT_CALL cannot detect if function was called)
 TEST_F(CommandsTest, SayCommandWorks) {
-    MockGameManager mockGameManager;
+    OnlineUserManager u;
+    CharacterManager c;
+    WorldManager w;
+    MockCommandSay mockCommandSay(c, u, w);
+    std::string commandToPut = "say: hello";
+    bool isCommandValid = true;
     testGameManager.extractCommands(
         testUserName,
         "create-account " + testUserName + " " + testUserPassword
@@ -55,52 +60,5 @@ TEST_F(CommandsTest, SayCommandWorks) {
         testUserName,
         "login " + testUserName + " " + testUserPassword
     );
-    EXPECT_CALL(mockGameManager, extractCommands(testUserName, testUserName));
-    // TODO: instead of checking if extractCommands is called, try to go deeper
-    testGameManager.extractCommands(
-        testUserName, testUserName
-    );
-
-
+    //EXPECT_CALL(mockCommandSay, reassembleCommand(commandToPut, isCommandValid)).Times(1);
 }
-
-
-/*
-TEST_F(CommandsTest, CommandsSuccessfullyRecognized) {
-    for (int i = 0; i < listOfCommands.size(); i++) {
-        EXPECT_NE(commandNotFoundString,
-            testGameManager.extractCommands(testUserName, listOfCommands[i]));
-    }
-}
-
-TEST_F(CommandsTest, SayCommand) {
-    MockGameManager mockGameManager;
-
-    std::vector<std::string> testList = {
-        "create-account " + testUserName + " " + testUserPassword,
-        "login " + testUserName + " " + testUserPassword,
-        "say: test",
-        " sAy : ssdw     a  test "
-    };
-
-    EXPECT_CALL(mockGameManager, extractCommands(const std::string& connectionID, std::string fullCommand))
-        .Times(4)
-        .WillOnce(Re)
-
-    testGameManager.extractCommands(
-        testUserName,
-        "create-account " + testUserName + " " + testUserPassword
-    );
-    testGameManager.extractCommands(
-        testUserName,
-        "login " + testUserName + " " + testUserPassword
-    );
-
-    EXPECT_EQ(commandUserNotOnlineString,  
-        testGameManager.extractCommands(testUserName, "say: test"));
-    EXPECT_EQ(commandUserNotOnlineString,
-        testGameManager.extractCommands(testUserName, " sAy : ssdw     a  test "));
-}
-*/
-// testGameManager.extractCommands(testUserName, "create-account " + testUserName + " " + testUserPassword);
-// testGameManager.extractCommands(testUserName, "login " + testUserName + " " + testUserPassword);
