fcb9735b23be394c9066e724da9faca70b3c5aea
mpuchkov <mpuchkov@sfu.ca>
2019-03-19 21:23:09 -0700

    Documentation: WIP headerdoc

---
+++ sh/doc.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#  doc.sh
+#  adventure2019
+#
+#  Created by admin on 2019-03-19.
+#  Copyright © 2019 Maxim Puchkov. All rights reserved.
+
+
+# headerdoc2html -o ../out/path/ ../
+# gatherheaderdoc ../out/path/
