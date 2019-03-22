e9ef662600498363467eff29b53b48751e1403f8
mpuchkov <mpuchkov@sfu.ca>
2019-03-21 19:56:54 -0700

    CustomActions design renamed to Actions

--- lib/object/include/Actions.h
+++ lib/object/include/Actions.h
@@ -1,44 +1,32 @@
 //
-//  CustomActions.h
-//  adventure2019: objects
-//  com.maximpuchkov.Faworki.adventure2019.CustomActions
+//  Actions.h
+//  adventure2019: adventure2019
+//  com.maximpuchkov.Faworki.adventure2019.Actions
 //
-//
-//  Created by mpuchkov with macOS 10.13, Xcode 10.1.
+//  Created by mpuchkov on 2019-03-21. macOS 10.13, Xcode 10.1.
 //  Copyright © 2019 Maxim Puchkov. All rights reserved.
 //
 
-#ifndef CustomActions_h
-#define CustomActions_h
+#ifndef Actions_h
+#define Actions_h
 
-#include <string>
 #include <map>
 
-
-/**
- @class CustomActions
- 
+/*
+ @class Actions
  @brief Defines action reference, for any
            custom action key or its alias
- 
- @ignore
  */
 template<typename KeyType, typename RefType>
 struct CustomActions {
 public:
-    
     CustomActions()
     : map({})
     { }
-    
 private:
-    
     std::map<KeyType, RefType> map;
-    
     KeyType key;
     RefType reference;
-    
 };
 
-
-#endif /* CustomActions_h */
+#endif /* Actions_h */
---
+++ lib/object/src/Actions.cpp
@@ -0,0 +1,10 @@
+//
+//  Actions.cpp
+//  adventure2019: adventure2019
+//  com.maximpuchkov.Faworki.adventure2019.Actions
+//
+//  Created by mpuchkov on 2019-03-21. macOS 10.13, Xcode 10.1.
+//  Copyright © 2019 Maxim Puchkov. All rights reserved.
+//
+
+#include <stdio.h>
