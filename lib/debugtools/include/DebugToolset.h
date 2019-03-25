f39255dbb0a839f11627e7957fe6781de58b874c
mpuchkov <mpuchkov@sfu.ca>
2019-03-25 10:53:23 -0700

    DebugToolset organized. Option to silence debug messages added.

--- lib/debugtools/include/DebugToolset.h
+++ lib/debugtools/include/DebugToolset.h
@@ -15,13 +15,25 @@
 
 namespace debug {
 
+/// Format customization
 inline namespace colors {
     const std::string RED = "\u001b[31m";
     const std::string RESET = "\u001b[0m";
 }
 
+/// Functionality customization
+inline namespace config {
+    static bool silenced = false;
+}
 
+/// Prefixes all outputted messages
+// Message:
+// "    Debug: <parameters>"
 static std::string PrefixMessage = "Debug";
+
+
+
+/* Debug Toolset */
 
 static inline void highlight() {
     std::cout << RED;
@@ -31,8 +43,16 @@ static inline void reset() {
     std::cout << RESET;
 }
 
+
+/*!
+ @function print
+ @param ts Variadic generic parameters
+ @discussion Universal function to print debug messages. Highlights messages
+             and allows disabling all messages.
+ */
 template<typename ...T>
 static void print(T &&...ts) {
+    if (silenced) { return; }
     std::cout << '\t' << debug::PrefixMessage << ": ";
     highlight();
     (std::cout << ... << ts) << '\n';
