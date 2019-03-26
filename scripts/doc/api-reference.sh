f1e913c25b091517b15fb007f367a27d547aaaae
mpuchkov <mpuchkov@sfu.ca>
2019-03-25 18:35:24 -0700

    API Reference shell script will keep all folders, including empty.

--- scripts/doc/api-reference.sh
+++ scripts/doc/api-reference.sh
@@ -4,9 +4,14 @@
 #  Generates 'doc/api-refence.html' from all .h headers in 'lib/' folder.
 
 
-#  Generate HTML files
+#  Set output path. Default path: 'doc/.HeaderDoc/'
 HTML_OUTPUT_PATH=doc/.HeaderDoc/
+
+#  Run 'headerdoc2html' to generate .html reference pages
 bin/headerdoc2html -o ${HTML_OUTPUT_PATH} $(find -E lib -regex '.*\.h')
 
-#  Generate main Table of Contents
+#  Create main Table of Contents from the generated .html files
 bin/gatherheaderdoc ./doc api-reference.html
+
+#  Keep all Headerdoc folders
+find ${HTML_OUTPUT_PATH} -type d -exec touch {}/.keep \;
