cc1f77594fcf70f731508f2e5f7f2208f111d2b1
mpuchkov <mpuchkov@sfu.ca>
2019-03-28 09:36:12 -0700

    New item files; deleted Identity.h/cpp

---
+++ lib/item/include/ItemController.h
@@ -0,0 +1,14 @@
+//
+//  ItemController.h
+//  adventure2019: adventure2019
+//  com.maximpuchkov.Faworki.adventure2019.ItemController
+//
+//  Created by mpuchkov on 2019-03-28. macOS 10.13, Xcode 10.1.
+//  Copyright © 2019 Maxim Puchkov. All rights reserved.
+//
+
+#ifndef ItemController_h
+#define ItemController_h
+
+
+#endif /* ItemController_h */
---
+++ lib/item/include/items.hpp
@@ -0,0 +1,15 @@
+//
+//  items.hpp
+//  Objects: Builder
+//  com.maximpuchkov.Objects.Objects.items
+//
+//  Created by mpuchkov on 2019-03-28. macOS 10.13, Xcode 10.1.
+//  Copyright © 2019 Maxim Puchkov. All rights reserved.
+//
+
+#ifndef items_hpp
+#define items_hpp
+
+#include <stdio.h>
+
+#endif /* items_hpp */
