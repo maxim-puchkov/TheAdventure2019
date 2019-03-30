ae971cdd79383f8c306887763e1f251c15f49060
mpuchkov <mpuchkov@sfu.ca>
2019-03-29 19:19:25 -0700

    CI: failing stage removed

---
+++ lib/item/include/ItemControllerDelegate.h
@@ -0,0 +1,14 @@
+//
+//  ItemControllerDelegate.h
+//  Objects: Objects
+//  com.maximpuchkov.Objects.Objects.ItemControllerDelegate
+//
+//  Created by mpuchkov on 2019-03-29. macOS 10.13, Xcode 10.1.
+//  Copyright © 2019 Maxim Puchkov. All rights reserved.
+//
+
+#ifndef ItemControllerDelegate_h
+#define ItemControllerDelegate_h
+
+
+#endif /* ItemControllerDelegate_h */
