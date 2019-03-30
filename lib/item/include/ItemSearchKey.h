b02e998713c6bd75f9e7eb0a8478753f0b09b204
mpuchkov <mpuchkov@sfu.ca>
2019-03-29 22:11:21 -0700

    Old test files removed

---
+++ lib/item/include/ItemSearchKey.h
@@ -0,0 +1,14 @@
+//
+//  ItemSearchKey.h
+//  Objects: Objects
+//  com.maximpuchkov.Objects.Objects.ItemSearchKey
+//
+//  Created by mpuchkov on 2019-03-29. macOS 10.13, Xcode 10.1.
+//  Copyright © 2019 Maxim Puchkov. All rights reserved.
+//
+
+#ifndef ItemSearchKey_h
+#define ItemSearchKey_h
+
+
+#endif /* ItemSearchKey_h */
