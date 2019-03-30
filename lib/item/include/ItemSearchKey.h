ee381175486b87ea90324bdc5c7e54d32b096f4d
mpuchkov <mpuchkov@sfu.ca>
2019-03-29 22:22:19 -0700

    Item: item's id or keywords can be used to search for an item; Cmake update

--- lib/item/include/ItemSearchKey.h
+++ lib/item/include/ItemSearchKey.h
@@ -10,5 +10,51 @@
 #ifndef ItemSearchKey_h
 #define ItemSearchKey_h
 
+#include "items.hpp"
+
+namespace items {
+
+
+    struct ItemSearchKey {
+        Identifier id;
+        Keywords keywords;
+        
+        bool operator==(const ItemSearchKey &other) const {
+            return ((this->id == other.id) || (this->keywords == other.keywords));
+        }
+        
+        bool operator==(ItemSearchKey &other) const {
+            return ((this->id == other.id) || (this->keywords == other.keywords));
+        }
+    };
+
+    
+} /* namespace std */
+
+
+
+
+
+
+namespace std {
+    
+    using items::ItemSearchKey;
+    
+    
+    /*!
+     @struct hash
+     @discussion Item's hash function
+     
+     @ignore
+     */
+    template<>
+    struct hash<ItemSearchKey> {
+        size_t operator()(const ItemSearchKey &key) const {
+            return hash<items::Identifier>{}(key.id);
+        }
+    };
+    
+    
+} /* namespace std */
 
 #endif /* ItemSearchKey_h */
---
+++ lib/item/src/ItemSearchKey.cpp
@@ -0,0 +1,10 @@
+//
+//  ItemSearchKey.cpp
+//  Objects: Objects
+//  com.maximpuchkov.Objects.Objects.ItemSearchKey
+//
+//  Created by mpuchkov on 2019-03-29. macOS 10.13, Xcode 10.1.
+//  Copyright © 2019 Maxim Puchkov. All rights reserved.
+//
+
+#include "ItemSearchKey.h"
