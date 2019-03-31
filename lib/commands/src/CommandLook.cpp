c269a6a77aa07f6487ec4cd7f34dd83b089f2823
Jordan <jha257@sfu.ca>
2019-03-31 04:47:07 -0700

    command for look added

--- lib/commands/src/CommandLook.cpp
+++ lib/commands/src/CommandLook.cpp
@@ -5,14 +5,46 @@
 using boost::lexical_cast;
 using boost::bad_lexical_cast;
 
-void CommandLook::executeInHeartbeat(const std::string& username, const std::vector<std::string>& fullCommand) {
-	auto location = characterManager.getCharacterLocation(username);
+void CommandLook::executeInHeartbeat(const std::string& userName, const std::vector<std::string>& fullCommand) {
+
+	auto location = characterManager.getCharacterLocation(userName);
+
+
     if(location.area == -1) {
         //should not reach here, report error
+        onlineUserManager.addMessageToUser(userName, "ERROR: this message should not print, invalid area for looking\n" );
         return;
     }
-    
-    onlineUserManager.addMessageToUser(username, (worldManager.look(location) + "\n"));
+
+
+    auto room = worldManager.findRoomByLocation(location);
+    std::string listOfExits = room.listExits();
+    const auto listUsers = room.getUserNames();
+
+    if(fullCommand.size() == 1){
+        std::string allUsersInRoom = "";
+        for(std::string userA : listUsers){
+            allUsersInRoom.append(userA + ",\n");
+        }
+
+        onlineUserManager.addMessageToUser(userName, (listOfExits + "\n" + allUsersInRoom + worldManager.look(location) ));
+        return;
+
+    }
+
+    /////////////////////////////////////////// If looking at a specific user then....
+    //ROUGH code debug later
+
+    std::string targetLookingAt = fullCommand.at(1);
+    const auto target = std::find(listUsers.begin(), listUsers.end() , targetLookingAt);
+    if(target != listUsers.end()){
+        onlineUserManager.addMessageToUser( userName, worldManager.look(location,targetLookingAt) );
+    } else{
+        onlineUserManager.addMessageToUser(userName, " user/object not found\n");
+    }
+
+    //////////////////////////////////////////
+
 }
 
 std::vector<std::string> CommandLook::reassembleCommand(std::string& fullCommand, bool& commandIsValid) {
@@ -25,6 +57,7 @@ std::vector<std::string> CommandLook::reassembleCommand(std::string& fullCommand
 
     //split by " " and compress all long spaces
     boost::split(processedCommand, fullCommand, boost::is_any_of(" \t"), boost::token_compress_on);
+
     if(processedCommand.size() == 3) {
         //check if [object-index] is a positive number
         try {
