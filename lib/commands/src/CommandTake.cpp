7807b6d2620acd9a240e83dbc4c1354b0bbd91cd
jha257 <jordan_hoang@sfu.ca>
2019-04-02 14:46:38 -0700

    commandTakemodify

--- lib/commands/src/CommandTake.cpp
+++ lib/commands/src/CommandTake.cpp
@@ -10,45 +10,51 @@ using boost::bad_lexical_cast;
 
 /// Execute command
 void CommandTake::executeInHeartbeat(const std::string& username, const std::vector<std::string>& fullCommand) { // const {
-    
+
     debug::prefix("TAKE");
-    
+
     // Find character's location
     auto location = Command::characterManager.getCharacterLocation(username);
 //
 //    // Then find the actual room
 //    // auto room = Command::worldManager.findRoomByLocation(location);
-//    
+//
     // Keyword is the second element of the vector
     auto keyword = fullCommand.at(1);
 
-    
-    auto items = Command::worldManager.items;
-    
-    
+
+///    auto items = Command::worldManager.items;
+
+
     // Note: location.room actually means the id of the room
     auto room_id = location.room;
-    
-    auto ids = items.search(room_id, keyword);
-    
-    
+    auto ids = Command::worldManager.items.search(room_id, keyword);
+
+
     // Note: need to add after fixing this
     auto character_id = 100;
-    
-    
+
+
     if (ids.size() == 1) {
         auto item_id = ids[0];
         // Command::worldManager.items.reassign(room_id, character_id, item_id);
+        print( "item id: ", item_id);
+
     } else {
         print("deal with more than one item");
     }
 
-    
+    print(keyword);
+    print(ids.size());
+    print(room_id);
+    print(location.area());
+
+
 //
 //    // Find all items matching this keyword
 //    // Searching whole world because rooms don't have items
 //    auto ids = Command::worldManager.items.search(keyword);
-//    
+//
 //    if (ids.size() == 1) {
 //        // If there is exactly one item matching
 //        auto itemID = ids[0];
@@ -57,21 +63,21 @@ void CommandTake::executeInHeartbeat(const std::string& username, const std::vec
 //    } else {
 //        // Else two or more items have the same keyword...
 //    }
-//    
-//    
-//    
-//    
+//
+//
+//
+//
 //    /* Debugging */
-//    
+//
 //    debug::prefix("CommandTake");
-//    
+//
 //    // Note: location.room actually means the id of the room
 //    // debug::print("Room: ", room.getName(), " (id: ", location.room, ")");
 //    debug::print("Keyword: ", keyword, '\n');
-//    
+//
 //    debug::print("Found ", ids.size(), " items matching the keyword");
 
-    
+
 }
 
 
@@ -96,6 +102,6 @@ std::vector<std::string> CommandTake::reassembleCommand(std::string& fullCommand
     } else {
         commandIsValid = (processedCommand.size() == 2);
     }
-   
+
     return processedCommand;
 }
