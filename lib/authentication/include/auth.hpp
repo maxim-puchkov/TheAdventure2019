9e350a1a4ca641250a4396e3146aab2ef84b24f9
mpuchkov <mpuchkov@sfu.ca>
2019-04-04 10:24:48 -0700

    auth.hpp

---
+++ lib/authentication/include/auth.hpp
@@ -0,0 +1,15 @@
+//
+//  auth.hpp
+//  adventure2019: adventure2019
+//  com.maximpuchkov.Faworki.adventure2019.auth
+//
+//  Created by mpuchkov on 2019-04-04. macOS 10.13, Xcode 10.1.
+//  Copyright © 2019 Maxim Puchkov. All rights reserved.
+//
+
+#ifndef auth_hpp
+#define auth_hpp
+
+#include <stdio.h>
+
+#endif /* auth_hpp */
