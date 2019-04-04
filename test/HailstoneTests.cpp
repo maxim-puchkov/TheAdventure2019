4b58867b15fcf7c81ecf041d926cc9741ba5ee22
mpuchkov <mpuchkov@sfu.ca>
2019-04-04 02:25:38 -0700

    Tests renamed; removed

--- test/HailstoneTests.cpp
+++ test/HailstoneTests.cpp
@@ -31,50 +31,9 @@ protected:
 
 
 // Test n == 0
-TEST_F(HailstoneTests, numIsZero) {
+TEST_F(HailstoneTests, NotTriangle) {
     int number = 0;
     EXPECT_EQ(0, number);
-}
-
-
-// Test n != 0
-TEST_F(HailstoneTests, numIsNonZero) {
-    int number = 4;
-    EXPECT_NE(0, number);
-}
-
-
-// Test n == 1
-TEST_F(HailstoneTests, numIsOne) {
-    int number = 1;
-    EXPECT_EQ(1, number);
-}
-
-
-// Test result
-TEST_F(HailstoneTests, returnValue) {
-    int number = 0;
-    EXPECT_EQ(!(number == 0), satisfiesHailstone(number));
-}
-
-
-// Test 'if-else' division
-TEST_F(HailstoneTests, division) {
-    int number = 6;
-    int next = computeNext(number);
-    
-    EXPECT_EQ(0, number % 2);
-    EXPECT_EQ(next, number / 2);
-}
-
-
-// Test 'else' multiplication
-TEST_F(HailstoneTests, multiplication) {
-    int number = 5;
-    int next = computeNext(number);
-    
-    EXPECT_EQ(1, number % 2);
-    EXPECT_EQ(next, 3 * number + 1);
 }
 
 
